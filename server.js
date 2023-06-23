aa1de24f499917a48493ad0da923df9b9a3b41a0
Edpow <e_dg_roch@hotmail.com>
2023-06-23 15:46:27 -0400

    Initial release

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+
+app.set("view engine","ejs");
+
+app.get("/", function(req,res){
+    const items =[
+        {
+            title: 'D',
+            message: 'ensenvolve aplicações de forma fácil'
+        },
+        {
+            title: 'E',
+            message: 'EJS usa JavaScript para renderizar HTML'
+        },
+        {
+            title: 'M',
+            message: 'uito fácil de usar'
+        },
+        {
+            title: 'A',
+            message: 'hhhhhhhh'
+        },
+        {
+            title: 'I',
+            message: 'tuitivo'
+        },
+        {
+            title: 'S',
+            message: 'intaxe simples'
+        },
+    ]
+
+    const subtitle = 'Uma linguagem de modelagem para criação de página HTML'
+    res.render("pages/index", {
+        qualitys:items,
+        subtitle:subtitle
+    });
+})
+
+app.get("/about", function(req,res){
+    res.render("pages/about");
+})
+
+app.listen(8080);
+console.log('Rodando...');
